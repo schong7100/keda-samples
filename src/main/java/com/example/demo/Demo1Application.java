2f4e36bb7a7b05baa988d3f7ec2fcfa3d74fcb21
sungchulhong <sungchulhong@localhost>
2022-11-29 15:05:58 +0900

    app-2

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,30 +2,43 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.Tag;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-	}
-	@RestController
-	public class SampleController {
-
-	    @GetMapping("/test")
-	    public String test() {
-	        List<Tag> tags = List.of(
-	                Tag.of("my_label_a", "A"),
-	                Tag.of("my_label_b", "B")
-	        );
-	        Metrics.counter("keda.demo.count.total", tags).increment();
-	        return "test";
-	    }
+		
 	}
 
+@RestController
+public class GaugeController {
+
+    private final Gauge queueSize;
+
+    public GaugeController(CollectorRegistry collectorRegistry) {
+        queueSize = Gauge.build()
+                .name("queue_size")
+                .help("Size of queue.")
+                .register(collectorRegistry);
+    }
+
+    @GetMapping(value = "/push")
+    public String push() {
+        queueSize.inc();
+        return "You pushed an item to the queue!";
+    }
+
+    @GetMapping(value = "/pop")
+    public String pop() {
+        queueSize.dec();
+        return "You popped an item from the queue!";
+    }
+}
 }
